1bc4595af7afbb106265622b590ef2719fad8549
Tsyriulnikov <max_djvu@mail.ru>
2022-03-04 14:43:21 +0300

    04_03_2022

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -11,16 +11,12 @@ type MessagePropsType = {
 function Message(props: MessagePropsType) {
     return (
         <div className={classNames.card}>
-            {/* <div><img className={classNames.card__img} src={props.avatar}/></div>*/}
-
-            <div className={classNames.card__message}>
+                <div className={classNames.card__message}>
+                <img className={classNames.card__img} src={props.avatar}/>
                 <div className={classNames.name}>{props.name}</div>
                 <div className={classNames.message}>{props.message}</div>
-                <div className={classNames.time}>{props.time}</div>            </div>
-            <div className={classNames.card__corner}> </div>
-            <div className={classNames.card__avatar}><img className={classNames.card__img} src={props.avatar}/></div>
-
-
+                <div className={classNames.time}>{props.time}</div>
+                </div>
         </div>
     )
 }
