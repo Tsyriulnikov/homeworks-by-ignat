207670b9744a4336f5783b2545fd7b8ca7f3b389
Tsyriulnikov <max_djvu@mail.ru>
2022-03-08 18:06:51 +0300

    1 final

--- src/p2-homeworks/h1/Message.module.css.d.ts
+++
@@ -1,9 +0,0 @@
-export
-const card: string
-const card__img:string
-const card__corner:string
-const card__message:string
-const card__avatar:string
-const name: string
-const message: string
-const time: string
