b630cc2504757967ed3088a610f698c2148535ce
Tsyriulnikov <max_djvu@mail.ru>
2022-03-30 14:16:32 +0300

    Updates

---
+++ precache-manifest.1e863d5fddbfc313f39802bc9ab6ca3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2815f5119764e6ad43ffed09aae14d4",
+    "url": "/homeworks-by-ignat/index.html"
+  },
+  {
+    "revision": "0c398dee6c6018fe209f",
+    "url": "/homeworks-by-ignat/static/css/main.05282eed.chunk.css"
+  },
+  {
+    "revision": "6f268a1f1ae883e6e3dd",
+    "url": "/homeworks-by-ignat/static/js/2.cd692d26.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/homeworks-by-ignat/static/js/2.cd692d26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c398dee6c6018fe209f",
+    "url": "/homeworks-by-ignat/static/js/main.2165e8e7.chunk.js"
+  },
+  {
+    "revision": "5b6fe110039571657cb3",
+    "url": "/homeworks-by-ignat/static/js/runtime-main.16506538.js"
+  },
+  {
+    "revision": "ef9e227efb9f9e76b05b676a1662aee7",
+    "url": "/homeworks-by-ignat/static/media/checked.ef9e227e.png"
+  }
+]);
