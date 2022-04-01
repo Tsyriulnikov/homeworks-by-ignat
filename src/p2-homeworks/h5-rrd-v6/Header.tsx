18481cdd0bf5c44f36e95a7d7063a501d707da78
Tsyriulnikov <max_djvu@mail.ru>
2022-04-01 13:24:48 +0300

    hw_5.2

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,28 +1,43 @@
 import React, {useState} from 'react'
-import {NavLink} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 import {PATH} from './Pages'
 import s from './Header.module.css'
+import * as FaIcons from 'react-icons/fa'
+import * as AiIcons from 'react-icons/ai';
+
 
 function Header() {
 
-    const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-    const toggleMenu = () => setIsMenuOpen((isOpen) => !isOpen);
+    // const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+    // const toggleMenu = () => setIsMenuOpen((isOpen) => !isOpen);
+    const [sidebar, setSidebar] = useState(false);
+    const showSidebar = () => setSidebar(!sidebar);
 
     return (
         <div className={s.container}>
 
 
-                <button className={s.nav__icon} onClick={toggleMenu}>Toggle Menu</button>
-
-                {isMenuOpen &&
-
-                    <div>
-                        <NavLink className={s.nav} to={'/'}>Pre_junior</NavLink>
-                        <NavLink className={s.nav} to={PATH.JUNIOR}>Junior</NavLink>
-                        <NavLink className={s.nav} to={PATH.JUNIOR_PLUS}>Junior+</NavLink>
-                    </div>
-                }
+            <div className={s.navbar}>
+                <Link to='#' className={s.menuBars}>
+                    <FaIcons.FaBars onClick={showSidebar}/>
+                </Link>
             </div>
+
+
+            <nav className={sidebar ? s.navMenuActive : s.navMenu}>
+                <ul className={s.navMenuItems} onClick={showSidebar}>
+                    <li className={s.navbarToggle}>
+                       <Link to='#' className={s.menuBars}>
+                            <AiIcons.AiOutlineClose/>
+                        </Link>
+                    </li>
+                    <NavLink className={s.nav} to={'/'}>Pre_junior</NavLink>
+                    <NavLink className={s.nav} to={PATH.JUNIOR}>Junior</NavLink>
+                    <NavLink className={s.nav} to={PATH.JUNIOR_PLUS}>Junior+</NavLink>
+                </ul>
+            </nav>
+
+        </div>
     )
 }
 
