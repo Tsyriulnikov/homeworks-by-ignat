baab715763e94f12d7f5e8650f7af18972e42180
Tsyriulnikov <max_djvu@mail.ru>
2022-05-11 15:12:35 +0300

    HW11 v0.5

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,24 +1,40 @@
-import React from 'react'
+import React, {DetailedHTMLProps, InputHTMLAttributes} from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 
-type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
-    // min, max, step, disable, ...
+ type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type SuperDoubleRangePropsType =DefaultInputPropsType & {
+    onChangeRange?: (value: number) => void
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
-        // min, max, step, disable, ...
+        type, // достаём и игнорируем чтоб нельзя было задать другой тип инпута
+        onChange, onChangeRange,
+        className,
+
+        ...restProps// все остальные пропсы попадут в объект restProps
     }
 ) => {
     // сделать самому, можно подключать библиотеки
 
+    const [value, setValue] = React.useState<number[]>([20, 37]);
+
+    const handleChange = (event: Event, newValue: number | number[]) => {
+        setValue(newValue as number[]);
+    };
+
     return (
-        <>
-            DoubleRange
-        </>
-    )
+        <Box sx={{ width: 300 }}>
+            <Slider
+                getAriaLabel={() => 'Temperature range'}
+                value={value}
+                onChange={handleChange}
+                valueLabelDisplay="auto"
+                // getAriaValueText={valuetext}
+            />
+        </Box>
+    );
 }
 
 export default SuperDoubleRange
